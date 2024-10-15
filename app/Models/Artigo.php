00b4ea3ec93d0e88f77570a8792310b95170afea
Pepe2HD <pedrohd977@gmail.com>
2024-10-15 19:38:28 -0300

    Add files via upload

---
+++ app/Models/Artigo.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Artigo extends Model
+{ 
+    use HasFactory;
+    
+    protected $table = 'article';
+   
+    protected $guarded = [];
+
+    protected $casts = [
+        'categories' => 'array'
+    ];
+
+    public function user(){
+        return $this->belongsTo('App\Models\User');
+    }
+
+    
+}
